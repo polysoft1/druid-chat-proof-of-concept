27255a1e030dcbcd8d8f9526e4786d2a0beb46e8
jaredoconnell <jared.oc321@gmail.com>
2022-12-01 22:25:05 -0500

    Added selector for various profile pic shapes

--- src/widgets/timeline_item.rs
+++ src/widgets/timeline_item.rs
@@ -1,6 +1,6 @@
-use druid::kurbo::Circle;
+use druid::kurbo::{Circle, RoundedRect};
 use druid::widget::prelude::*;
-use druid::{Widget, WidgetExt, widget, ImageBuf};
+use druid::{Widget, WidgetExt, widget};
 use druid::piet::{Color, kurbo};
 use druid::WidgetPod;
 use druid::Point;
@@ -11,13 +11,21 @@ pub struct TimelineItemWidget {
     label: WidgetPod<Message, widget::Container<Message>>,
 }
 
-const PROFILE_PIC_WIDTH: f64 = 30.0;
+const PROFILE_PIC_WIDTH: f64 = 35.0;
 const PROFILE_PIC_SPACING: f64 = 3.5;
 const PROFILE_PIC_AREA: f64 = PROFILE_PIC_WIDTH + PROFILE_PIC_SPACING;
 const MSG_COLOR: Color = Color::rgb8(75, 75, 76);
 const ARROW_SIZE: f64 = 7.0;
 const ARROW_X: f64 = PROFILE_PIC_AREA;
-const CURVE_PATH: bool = false;
+const CURVE_PATH: bool = true;
+const PICTURE_SHAPE: PictureShape = PictureShape::Circle;
+pub enum PictureShape {
+    Rectangle,
+    RoundedRectangle,
+    Circle,
+    Hexagon,
+    Octagon,
+}
 
 fn make_arrow_path() -> kurbo::BezPath {
     let mut path = kurbo::BezPath::new();
@@ -35,6 +43,40 @@ fn make_arrow_path() -> kurbo::BezPath {
     // To right to cover the curve of the bubble. Double size to ensure coverage of bubble.
     path.line_to(Point::new(ARROW_X + ARROW_SIZE * 2.0, 0.0));
     path.line_to(Point::new(ARROW_X, 0.0));
+    path.close_path();
+    path
+}
+fn make_hexagon_path(vertical_trim: f64, inset: f64) -> kurbo::BezPath {
+    let mut path = kurbo::BezPath::new();
+    let second_x = PROFILE_PIC_WIDTH * inset;
+    let third_x = PROFILE_PIC_WIDTH * (1.0 - inset);
+    let top_y = PROFILE_PIC_WIDTH * vertical_trim;
+    let middle_y = PROFILE_PIC_WIDTH / 2.0;
+    let bottom_y = PROFILE_PIC_WIDTH * (1.0 - vertical_trim);
+    path.move_to(Point::new(0.0, middle_y)); // Start
+    path.line_to(Point::new( second_x, top_y));
+    path.line_to(Point::new( third_x, top_y));
+    path.line_to(Point::new( PROFILE_PIC_WIDTH, middle_y));
+    path.line_to(Point::new( third_x, bottom_y));
+    path.line_to(Point::new( second_x, bottom_y));
+    path.line_to(Point::new(0.0, middle_y));
+    path.close_path();
+    path
+}
+
+fn make_octagon_path(fraction_from_corner: f64) -> kurbo::BezPath {
+    let dist_from_corner = PROFILE_PIC_WIDTH * fraction_from_corner;
+    let other_side_pos = PROFILE_PIC_WIDTH - dist_from_corner;
+
+    let mut path = kurbo::BezPath::new();
+    path.move_to(Point::new(0.0, dist_from_corner)); // Start
+    path.line_to(Point::new( dist_from_corner, 0.0));
+    path.line_to(Point::new( other_side_pos, 0.0));
+    path.line_to(Point::new( PROFILE_PIC_WIDTH, dist_from_corner));
+    path.line_to(Point::new( PROFILE_PIC_WIDTH, other_side_pos));
+    path.line_to(Point::new( other_side_pos, PROFILE_PIC_WIDTH));
+    path.line_to(Point::new( dist_from_corner, PROFILE_PIC_WIDTH));
+    path.line_to(Point::new( 0.0, other_side_pos));
     path.close_path();
     path
 }
@@ -112,13 +154,19 @@ impl Widget<Message> for TimelineItemWidget {
             let image_data = data.profile_pic.clone();
             image_data.to_image(ctx.render_ctx)
         };
-        ctx.with_save(|ctx| {
-                ctx.clip(Circle::new(Point::new(PROFILE_PIC_WIDTH / 2.0, PROFILE_PIC_WIDTH / 2.0), PROFILE_PIC_WIDTH / 2.0));
-                ctx.draw_image(&piet_image, 
-                    druid::Rect::new(0.0, 0.0, PROFILE_PIC_WIDTH, PROFILE_PIC_WIDTH),
-                     druid::piet::InterpolationMode::Bilinear
-                );
-            });
+        ctx.with_save(|ctx| { // Makes it so the clip doesn't mess up the following draws
+            match PICTURE_SHAPE {
+                PictureShape::Rectangle => {},
+                PictureShape::RoundedRectangle => ctx.clip(RoundedRect::new(0.0, 0.0, PROFILE_PIC_WIDTH, PROFILE_PIC_WIDTH, 6.0)),
+                PictureShape::Circle => ctx.clip(Circle::new(Point::new(PROFILE_PIC_WIDTH / 2.0, PROFILE_PIC_WIDTH / 2.0), PROFILE_PIC_WIDTH / 2.0)),
+                PictureShape::Hexagon => ctx.clip(make_hexagon_path(0.08, 0.25)),
+                PictureShape::Octagon => ctx.clip(make_octagon_path(0.25)),
+            }
+            ctx.draw_image(&piet_image, 
+                druid::Rect::new(0.0, 0.0, PROFILE_PIC_WIDTH, PROFILE_PIC_WIDTH),
+                    druid::piet::InterpolationMode::Bilinear
+            );
+        });
         // Now the little arrow that goes from the image to the bubble
         ctx.fill(make_arrow_path(), &MSG_COLOR);
     }
